ebf8108ea09842c3598f2d15b8e243a32ac0f6cb
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-14 08:20:01 +0530

    Creating a tools LB

---
+++ modules/alb/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+  default = "vpc-0468c874d427a36de"
+}
+
+variable "acm_arn" {
+  default = "arn:aws:acm:us-east-1:739561048503:certificate/cd6d0623-15f3-489a-aae1-badb324eafe7"
+}
