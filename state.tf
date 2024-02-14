9c6e7c238b85f7e482a5aaca7f594c27e1536447
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-14 07:36:48 +0530

    Making Changes to Mis code, Supporing more DRY

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "d77-terraform"
-    key    = "misc/sonarqube/terraform.tfstate"
+    key    = "misc-code/all/terraform.tfstate"
     region = "us-east-1"
 
   }
